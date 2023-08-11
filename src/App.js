43086eb6a9700ff4a13abe67cfed18cc4131f9c0
Jayshree Gupta <jayshreeg333@gmail.com>
2023-08-11 18:11:36 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Resturant from "./component/Basics/Resturant";
+import Navbar from './component/Basics/Navbar';
+import MenuCard from './component/Basics/MenuCard';
+import menuApi from './component/Basics/menuApi';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const App = () => {
+ return (
+  <>
+  <Resturant />
+  
+  </>
+ )
+   
+};
+
+export default App
+
+
+
